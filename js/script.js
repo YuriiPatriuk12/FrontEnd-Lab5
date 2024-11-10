b2b4bd11618f8a528082d5a3763b1a9ceecdfb76
IP-22PatriukYurii <Patriuk.Yurii@LLL.kpi.ua>
2024-11-10 19:57:51 +0200

    Complete second task

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* Task 1 */
 const fullNameRegex = /^[А-Яа-яЇїІіЄєҐґ]+ [А-ЯЇІЄҐ]\.[А-ЯЇІЄҐ]\.$/;
 const idCardRegex = /^[А-ЯЇІЄҐ]{2} №\d{6}$/;
 const dobRegex = /^\d{2}\.\d{2}\.\d{4}$/;
@@ -28,8 +29,43 @@ function validateForm() {
   const isEmailValid = validateField(email, emailRegex);
 
   if (isFullNameValid && isIdCardValid && isDobValid && isAddressValid && isEmailValid) {
-    alert(`Інформація введена коректно:\n\nПІБ: ${fullName.value}\nID-картка: ${idCard.value}\nДата народження: ${dob.value}\nАдреса: ${address.value}\nE-mail: ${email.value}`);
+    alert(`Інформація введена коректно:\n\nПІБ: ${fullName.value}\nID-карта: ${idCard.value}\nДата народження: ${dob.value}\nАдреса: ${address.value}\nE-mail: ${email.value}`);
   } else {
     alert("Будь ласка, перевірте правильність заповнення полів.");
   }
+}
+
+/* Task 2 */
+const table = document.getElementById("table");
+const colorPicker = document.getElementById("colorPicker");
+
+const cellWithNumber2 = [...table.getElementsByTagName("td")].find(cell => cell.textContent === "2");
+
+cellWithNumber2.addEventListener("mouseover", () => {
+  cellWithNumber2.style.backgroundColor = getRandomColor();
+});
+
+cellWithNumber2.addEventListener("click", () => {
+  cellWithNumber2.style.backgroundColor = colorPicker.value;
+});
+
+cellWithNumber2.addEventListener("dblclick", () => {
+  const colIndex = cellWithNumber2.cellIndex;
+  changeColumnColor(colIndex, colorPicker.value);
+});
+
+
+function changeColumnColor(colIndex, color) {
+  for (let row of table.rows) {
+    row.cells[colIndex].style.backgroundColor = color;
+  }
+}
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
